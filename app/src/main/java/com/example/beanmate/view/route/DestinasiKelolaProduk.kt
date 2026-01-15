a1ab93649a04acb43d088895b04dc21115a10b18
aldys002 <igidiaaldys@gmail.com>
2026-01-15 22:53:09 +0700

    menambahkan destinasi kelola produk

---
+++ app/src/main/java/com/example/beanmate/view/route/DestinasiKelolaProduk.kt
@@ -0,0 +1,8 @@
+package com.example.beanmate2.view.route
+
+import com.example.beanmate2.R
+
+object DestinasiKelolaProduk : DestinasiNavigasi {
+    override val route = "kelola_produk"
+    override val titleRes = R.string.app_name
+}
