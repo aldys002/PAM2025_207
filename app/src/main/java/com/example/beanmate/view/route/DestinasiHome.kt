ae27175551bb9ae40e06d7027e90340d11255004
aldys002 <igidiaaldys@gmail.com>
2026-01-15 22:53:22 +0700

    menambahkan destinasi home

---
+++ app/src/main/java/com/example/beanmate/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.beanmate2.view.route
+
+import com.example.beanmate2.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
