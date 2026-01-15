cff28fad0bfd81e9e6f17b51574d0238f399aa53
aldys002 <igidiaaldys@gmail.com>
2026-01-15 22:54:45 +0700

    menambahkan destinasi cart

---
+++ app/src/main/java/com/example/beanmate/view/route/DestinasiCart.kt
@@ -0,0 +1,8 @@
+package com.example.beanmate2.view.route
+
+import com.example.beanmate2.R
+
+object DestinasiCart : DestinasiNavigasi {
+    override val route = "cart"
+    override val titleRes = R.string.app_name
+}
