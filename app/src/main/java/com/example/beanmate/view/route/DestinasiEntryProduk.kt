59f64932dec8cea4b5e85b34a70e2d3cbedc980d
aldys002 <igidiaaldys@gmail.com>
2026-01-15 22:53:48 +0700

    menambahkan destinasi entry produk

---
+++ app/src/main/java/com/example/beanmate/view/route/DestinasiEntryProduk.kt
@@ -0,0 +1,8 @@
+package com.example.beanmate2.view.route
+
+import com.example.beanmate2.R
+
+object DestinasiEntryProduk : DestinasiNavigasi {
+    override val route = "entry_produk"
+    override val titleRes = R.string.form_title_add
+}
