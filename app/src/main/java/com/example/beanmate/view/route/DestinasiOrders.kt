d098d48f2eb8bc5a8b84a7a4f79122695fca520c
aldys002 <igidiaaldys@gmail.com>
2026-01-15 22:51:03 +0700

    menambahkan destinasi order de

---
+++ app/src/main/java/com/example/beanmate/view/route/DestinasiOrders.kt
@@ -0,0 +1,8 @@
+package com.example.beanmate2.view.route
+
+import com.example.beanmate2.R
+
+object DestinasiOrders : DestinasiNavigasi {
+    override val route = "orders"
+    override val titleRes = R.string.app_name
+}
