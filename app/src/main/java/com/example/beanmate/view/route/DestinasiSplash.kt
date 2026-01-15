e4282708eee3ef73f15a866c1715dc0400a1d85c
aldys002 <igidiaaldys@gmail.com>
2026-01-15 22:50:41 +0700

    menambahkan destinasi order

---
+++ app/src/main/java/com/example/beanmate/view/route/DestinasiSplash.kt
@@ -0,0 +1,8 @@
+package com.example.beanmate2.view.route
+
+import com.example.beanmate2.R
+
+object DestinasiSplash : DestinasiNavigasi {
+    override val route = "splash"
+    override val titleRes = R.string.app_name
+}
