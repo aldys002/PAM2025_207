3ab59f688851e9aac26085ff8059abb9aa751676
aldys002 <igidiaaldys@gmail.com>
2026-01-15 22:54:30 +0700

    menambahkan destinasi checkout

---
+++ app/src/main/java/com/example/beanmate/view/route/DestinasiCheckout.kt
@@ -0,0 +1,8 @@
+package com.example.beanmate2.view.route
+
+import com.example.beanmate2.R
+
+object DestinasiCheckout : DestinasiNavigasi {
+    override val route = "checkout"
+    override val titleRes = R.string.checkout_title
+}
