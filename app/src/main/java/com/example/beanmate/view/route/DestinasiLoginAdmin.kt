81aaa5084253c9f160678324a81715463e7a9f91
aldys002 <igidiaaldys@gmail.com>
2026-01-15 22:52:42 +0700

    menambahkan destinasi login admin

---
+++ app/src/main/java/com/example/beanmate/view/route/DestinasiLoginAdmin.kt
@@ -0,0 +1,8 @@
+package com.example.beanmate2.view.route
+
+import com.example.beanmate2.R
+
+object DestinasiLoginAdmin : DestinasiNavigasi {
+    override val route = "login_admin"
+    override val titleRes = R.string.app_name
+}
